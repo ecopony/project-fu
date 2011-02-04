c4a7318d09a4f85f318ee2321ef10f3f892e46ae
Ed Copony <ecopony@yahoo.com>
2011-02-03 22:39:39 -0800

    User model bits for project ownership status and eligibility to be added to a project.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,18 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+
+  class << self
+
+    def eligible_for_membership_to(project)
+      raise if (project.nil? || !project.is_a?(Project))
+      project_member_ids = project.project_member_ids
+      User.order("email ASC").reject { |user| project_member_ids.include?(user.id) }
+    end
+
+  end
+
+  def is_a_owner_of?(project)
+    project.owners.include?(self)
+  end
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,47 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe User do
+
+  describe "project-related methods" do
+    before(:each) do
+      @project = Factory.create(:project)
+      @project_creator = @project.creator
+      @project_member = Factory.create(:user)
+      @project.project_memberships.create(:user => @project_member, :role => "member")
+      @non_member = Factory.create(:user)
+    end
+
+    describe "project-membership eligibility" do
+
+      it "should raise an error if it receives nil as an argument" do
+        lambda { User.eligible_for_membership_to(nil) }.should raise_error(RuntimeError)
+      end
+
+      it "should raise an error if the object it recieves is not a project" do
+        lambda { User.eligible_for_membership_to(Factory.create(:user)) }.should raise_error(RuntimeError)
+      end
+
+      it "return users not already a member of the project" do
+        users = User.eligible_for_membership_to(@project)
+        users.size.should == 1
+        users.include?(@project_creator).should be_false
+        users.include?(@project_member).should be_false
+        users.include?(@non_member).should be_true
+      end
+    end
+
+    describe "project ownership status" do
+
+      it "should return true if the user is an owner of the project" do
+        @project_creator.is_a_owner_of?(@project).should be_true
+      end
+
+      it "should return false if the user is not an owner of the project" do
+        @project_member.is_a_owner_of?(@project).should be_false
+        @non_member.is_a_owner_of?(@project).should be_false
+      end
+    end
+
+  end
+
+end
